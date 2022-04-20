441d1260d201b1e8ab73924c7493fcf68c5549a0
Laura Buffington <laura.c.buffington@gmail.com>
2022-04-20 15:10:20 -0700

    write add, mark and removeTodo hooks

--- src/App.js
+++ src/App.js
@@ -11,12 +11,33 @@ function App() {
     }
   ]);
 
+  //use spread operator to append text to the list of todos
   const addTodo = text => {
     const newTodos = [...todos, {text}];
     setTodos(newTodos);
   }
+
+  //spread operator to create copy of todos
+  //use index to mark a todo, reassign isDone to true
+  const markTodo = index => {
+    const newTodos = [...todos];
+    newTodos[index].isDone = true;
+    setTodos(newTodos);
+  }
+
+  //takes in index, splice the list of todos with that index, set new todos.
+  const removeTodo = index => {
+    const newTodos = [...todos];
+    newTodos.splice(index, 1);
+    setTodos(newTodos);
+  }
+
   return (
-    <div>Hello</div>
+    <div className="app">
+      <div className="container">
+        <h1>Todo List</h1>
+      </div>
+    </div>
   )
 }
 
